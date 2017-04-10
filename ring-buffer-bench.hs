7789420e0ca9725e3ec56f69725b641dc56778e3
Ertugrul Söylemez <esz@posteo.de>
2017-04-10 22:41:57 +0200

    Add ring buffer benchmark

---
+++ ring-buffer-bench.hs
@@ -0,0 +1,126 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Main where
+
+import Control.Monad
+import Control.Monad.Primitive
+import Data.Primitive.MutVar
+import Data.Proxy
+import Data.Sequence (ViewL(..), (|>), viewl)
+import qualified Data.Sequence as Seq
+import qualified Data.Vector.Generic.Mutable as Vm
+import qualified Data.Vector.Mutable as Vbm
+import qualified Data.Vector.Unboxed.Mutable as Vum
+import System.Environment
+import System.Random.MWC
+import System.Random.MWC.Distributions
+
+
+-- Ring buffer version -----------------------------------------------
+
+data RingBuffer s v a =
+    RingBuffer {
+      _rbStart :: !(MutVar s Int),
+      _rbLen   :: !(MutVar s Int),
+      _rbVec   :: v s a
+    }
+
+type RingBufferIO = RingBuffer (PrimState IO)
+
+
+rbNew :: (PrimMonad m, Vm.MVector v a) => Int -> m (RingBuffer (PrimState m) v a)
+rbNew n = do
+    _rbStart <- newMutVar 0
+    _rbLen <- newMutVar 0
+    _rbVec <- Vm.new n
+    pure RingBuffer{..}
+
+
+rbPop :: (PrimMonad m, Vm.MVector v a) => RingBuffer (PrimState m) v a -> m (Maybe a)
+rbPop RingBuffer{..} = do
+    let len = Vm.length _rbVec
+    n <- readMutVar _rbLen
+    if n <= 0
+      then pure Nothing
+      else do
+          writeMutVar _rbLen $! n - 1
+
+          i'' <- readMutVar _rbStart
+          let i' = i'' + 1
+              i | i' >= len = 0
+                | otherwise = i'
+
+          writeMutVar _rbStart $! i
+          Just <$> Vm.read _rbVec i''
+
+
+rbPush :: (PrimMonad m, Vm.MVector v a) => RingBuffer (PrimState m) v a -> a -> m ()
+rbPush RingBuffer{..} x = do
+    let len = Vm.length _rbVec
+    i' <- readMutVar _rbStart
+    n' <- readMutVar _rbLen
+
+    let i | i' <= 0   = len - 1
+          | otherwise = i' - 1
+        n | n' >= len = len
+          | otherwise = n' + 1
+
+    writeMutVar _rbStart $! i
+    writeMutVar _rbLen $! n
+    Vm.write _rbVec i x
+
+
+rbToList :: (PrimMonad m, Vm.MVector v a) => RingBuffer (PrimState m) v a -> m [a]
+rbToList RingBuffer{..} = do
+    let m = Vm.length _rbVec - 1
+    i0 <- readMutVar _rbStart
+    n  <- readMutVar _rbLen
+    mapM (Vm.read _rbVec) . take n $
+        iterate (\i -> if i >= m then 0 else i + 1) i0
+
+
+rbBench :: forall m proxy v. (PrimMonad m, Vm.MVector v Int) => proxy v -> Gen (PrimState m) -> Int -> m ()
+rbBench _ rng n = do
+    rb <- rbNew 1000 :: m (RingBuffer (PrimState m) v Int)
+    replicateM_ n $ do
+        wantPush <- bernoulli 0.7 rng
+        if wantPush
+          then do x <- uniform rng; rbPush rb x
+          else rbPop rb >>= \mx -> maybe id seq mx (pure ())
+
+
+-- Seq version -------------------------------------------------------
+
+seqBench :: (PrimMonad m) => Gen (PrimState m) -> Int -> m ()
+seqBench rng = go mempty
+    where
+    go _ 0 = pure ()
+    go xs'' n = do
+        wantPush <- bernoulli 0.7 rng
+        if wantPush
+          then do
+              x :: Int <- uniform rng
+              let xs | Seq.length xs'' >= 1000 =
+                         case viewl xs'' of
+                           EmptyL   -> xs''
+                           _ :< xs' -> xs' |> x
+                     | otherwise = xs'' |> x
+              xs `seq` go xs (n - 1)
+          else
+              case viewl xs'' of
+                EmptyL  -> go xs'' (n - 1)
+                _ :< xs -> go xs (n - 1)
+
+
+main :: IO ()
+main = do
+    nStr : variant : _ <- getArgs
+    let n = read nStr
+    rng <- create
+    case variant of
+      "s"  -> seqBench rng n
+      "vb" -> rbBench (Proxy :: Proxy Vbm.MVector) rng n
+      "vu" -> rbBench (Proxy :: Proxy Vum.MVector) rng n
+      _    -> pure ()
